e1eb2c1e4642baa0713423613850127feaea25fa
Vincent Verbrugh <vincent@verbrugh.nl>
2024-02-19 22:20:38 +0100

    fix: day one

--- src/Day01.hs
+++ src/Day01.hs
@@ -8,19 +8,25 @@ firstAndLastDigit :: String -> (Char, Char)
 firstAndLastDigit str = case [d | d <- str, isDigit d] of
     ds -> (head ds, last ds)
 
-
 concatAndRead :: (Char, Char) -> Int
 concatAndRead (a, b) = read [a, b]
 
 digitLookupMap :: [(String, String)]
 digitLookupMap = [("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")]
 
-replaceDigits :: String -> String
-replaceDigits [] = []
-replaceDigits str = case [ (newStr, rest) | (oldStr, newStr) <- digitLookupMap, oldStr `isPrefixOf` str,
-                    let rest = drop (length oldStr) str ] of
-                    ((newStr, rest):_) -> newStr ++ replaceDigits rest
-                    [] -> head str : replaceDigits (tail str)
+convertDigits :: String -> String
+convertDigits "" = ""
+convertDigits line@(c : xs)
+    | "one"   `isPrefixOf` line = '1' : convertDigits xs
+    | "two"   `isPrefixOf` line = '2' : convertDigits xs
+    | "three" `isPrefixOf` line = '3' : convertDigits xs
+    | "four"  `isPrefixOf` line = '4' : convertDigits xs
+    | "five"  `isPrefixOf` line = '5' : convertDigits xs
+    | "six"   `isPrefixOf` line = '6' : convertDigits xs
+    | "seven" `isPrefixOf` line = '7' : convertDigits xs
+    | "eight" `isPrefixOf` line = '8' : convertDigits xs
+    | "nine"  `isPrefixOf` line = '9' : convertDigits xs
+    | otherwise                 =  c  : convertDigits xs
 
 day01 :: IO ()
 day01 = do
@@ -28,15 +34,17 @@ day01 = do
   let calibrationValues = map concatAndRead (map firstAndLastDigit inputLines)
  
   putStrLn "All calibration values:"
-  mapM_ print calibrationValues
+  -- mapM_ print calibrationValues
 
   putStrLn "Sum of all calibration values:"
-  print $ sum calibrationValues
+  -- print $ sum calibrationValues
 
-  let realCalibrationValues = map concatAndRead (map firstAndLastDigit ( map replaceDigits inputLines ))
+  -- print $ map replaceDigits inputLines
+
+  let realCalibrationValues = map concatAndRead (map firstAndLastDigit ( map convertDigits inputLines ))
 
   putStrLn "All real calibration values:"
-  mapM_ print realCalibrationValues
+  -- mapM_ print realCalibrationValues
 
   putStrLn "Sum of all real calibration values:"
   print $ sum realCalibrationValues
